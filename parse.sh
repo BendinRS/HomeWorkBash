02fb31d132c49e5a941901a40d3a91f75e73af5c
BendinRS <roman.bendin2013@ya.ru>
2022-04-15 17:36:49 +0300

    cron

--- parse.sh
+++ parse.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sh /home/HomeWorkBash/scriptXip.sh > parse.txt
-sh /home/HomeWorkBash//scriptYurl.sh >> parse.txt
-sh /home/HomeWorkBash//error.sh >> parse.txt
+sh /home/HomeWorkBash/scriptYurl.sh >> parse.txt
+sh /home/HomeWorkBash/error.sh >> parse.txt
 
 cat parse.txt
 rm parse.txt
